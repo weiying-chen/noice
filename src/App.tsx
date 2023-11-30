8b0010b61cf7a72ed6c03aa0e447981b02959649
Alex <alexchen.info@gmail.com>
2023-11-30 22:36:17 +0800

    Make play control work again

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, createRef, forwardRef } from 'react';
 import AudioSlider from './components/AudioSlider';
 import Control from './components/Control';
 // import useAudio from './hooks/useAudio';
@@ -10,95 +10,95 @@ const audios = [
   { src: '/fire.mp3', icon: '\\f06d' },
   { src: '/crickets.mp3', icon: '\\e4d0' },
   { src: '/birds.mp3', icon: '\\f520' },
-  { src: '/wind.mp3', icon: '\\f72e' },
-  { src: '/rain.mp3', icon: '\\f73d' },
-  { src: '/river.mp3', icon: '\\f773' },
-  { src: '/waves.mp3', icon: '\\f5ca' },
+  // { src: '/wind.mp3', icon: '\\f72e' },
+  // { src: '/rain.mp3', icon: '\\f73d' },
+  // { src: '/river.mp3', icon: '\\f773' },
+  // { src: '/waves.mp3', icon: '\\f5ca' },
 ];
 
-const defaultSliderValue = 50
+const useAudioPlayer = (audios, options) => {
+  const [isPlayingAudio, setIsAudioPlaying] = useState(false);
 
-function App() {
-  const audioRefs = audios.map(() => useRef(null));
-  const [isPlayingAudio, setIsPlayingAudio] = useState(false);
-
-  const { sliderValues, setSliderValues, handleSliderChange } = useSlider(
-    audios,
-    audioRefs,
-    defaultSliderValue
+  const audioRefs = useRef(
+    audios.map((audio) => ({
+      ...audio,
+      ref: createRef(),
+    })),
   );
 
   function playAudio() {
     if (!isPlayingAudio) {
-      audioRefs.forEach((audioRef) => {
-        if (audioRef.current) {
-          audioRef.current.play();
-        }
-      });
-
-      setIsPlayingAudio(true);
+      if (audioRefs && audioRefs.current[0]) {
+        console.log('volume:', audioRefs.current[0].ref.current.volume);
+      }
+      audioRefs.current?.forEach((audioRef) => audioRef.ref.current.play());
+      setIsAudioPlaying(true);
     } else {
-      audioRefs.forEach((audioRef) => {
-        if (audioRef.current) {
-          audioRef.current.pause();
-        }
-      });
-
-      setIsPlayingAudio(false);
+      audioRefs.current?.forEach((audioRef) => audioRef.ref.current?.pause());
+      setIsAudioPlaying(false);
     }
   }
 
   function resetAudio() {
-    setSliderValues((prevSliderValues) =>
-      prevSliderValues.map(() => defaultSliderValue)
-    );
-
-    audioRefs.forEach((audioRef, index) => {
+    audioRefs.current?.forEach((audioRef, index) => {
       if (audioRef.current) {
-        audioRef.current.volume = defaultSliderValue / 100;
+        audioRef.current.volume = options.defaultVolume / 100;
       }
     });
   }
 
-  function amplifyAudio() {
-    const increasedSliderValue = 20
+  return {
+    isPlayingAudio,
+    playAudio,
+    resetAudio,
+    audioRefs: audioRefs.current,
+  };
+};
 
-    setSliderValues((prevSliderValues) => 
-      prevSliderValues.map((prevSliderValue) => {
-        return prevSliderValue + increasedSliderValue;
-      })
-    );
+const defaultSliderValue = 50;
 
-    audioRefs.forEach((audioRef, index) => {
-      if (audioRef.current) {
-        audioRef.current.volume =
-          audioRef.current.volume + (increasedSliderValue / 100);
-      }
-    });
+const Audio = forwardRef((props, ref) => {
+  // const { src, icon, ref } = audioRef;
+  const { src, icon } = props;
+
+  function handleVolumeChange(value) {
+    ref.current.volume = value / 100;
+    // ref.current.volume = 1;
   }
+
+  return (
+    <div className="audio">
+      <audio ref={ref} loop controls>
+        <source src={src} type="audio/mpeg" /> Your browser does
+        not support the audio element.
+      </audio>
+      <div className="slider-wrapper">
+        <AudioSlider
+          min={0}
+          max={100}
+          step={1}
+          value={ref.current?.volume}
+          onChange={handleVolumeChange}
+          icon={icon}
+          vertical
+        />
+      </div>
+    </div>
+  );
+});
+
+function App() {
+  const { isPlayingAudio, playAudio, resetAudio, audioRefs } =
+
+  useAudioPlayer(audios, {
+    defaultVolume: defaultSliderValue,
+  });
 
   return (
     <>
       <div className="audios">
-        {audios.map((audio, index) => (
-          <div key={index} className="audio">
-            <audio ref={audioRefs[index]} loop>
-              <source src={audio.src} type="audio/mpeg" />
-              Your browser does not support the audio element.
-            </audio>
-            <div className="slider-wrapper">
-              <AudioSlider
-                min={0}
-                max={100}
-                step={1}
-                value={sliderValues[index]}
-                onChange={(newValue) => handleSliderChange(index, newValue)}
-                icon={audio.icon}
-                vertical
-              />
-              <p>{sliderValues[index]}%</p>
-            </div>
-          </div>
+        {audioRefs.map((audioRef, index) => (
+          <Audio key={audioRef.src} {...audioRef} />
         ))}
       </div>
       <div className="controls">
@@ -107,7 +107,6 @@ function App() {
           icon={isPlayingAudio ? "\\f04c" : "\\f04b"}
         />
         <Control onClick={resetAudio} icon="\f2f9" />
-        <Control onClick={amplifyAudio} icon="\f028" />
       </div>
     </>
   );
