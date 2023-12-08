351aa0a52be81a25771568c72cabcba2dcff4197
Alex <alexchen.info@gmail.com>
2023-12-08 15:49:12 +0800

    Restore all audios

--- src/App.tsx
+++ src/App.tsx
@@ -16,11 +16,21 @@ const icon = {
   volumeHigh: '\\f028',
   volumeLow: '\\f027',
   volumeOff: '\\f026',
+  crow: '\\f520',
+  wind: '\\f72e',
+  cloudRain: '\\f73d',
+  water: '\\f773',
+  umbrellaBeach: '\\f5ca',
 };
 
 const audios = [
   { name: 'Fire', src: '/fire.mp3', icon: icon.fire },
   { name: 'Crickets', src: '/crickets.mp3', icon: icon.bugs },
+  { name: 'Birds', src: 'birds.mp3', icon: icon.crow },
+  { name: 'Wind', src: '/wind.mp3', icon: icon.wind },
+  { name: 'Rain', src: '/rain.mp3', icon: icon.cloudRain },
+  { name: 'River', src: '/river.mp3', icon: icon.water },
+  { name: 'Sea', src: '/waves.mp3', icon: icon.umbrellaBeach },
 ];
 
 const DEFAULT_VOLUME = 0;
