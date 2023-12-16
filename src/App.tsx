67068e5fbfeaefc84dffc8129af15fb3fbec9085
Alex <alexchen.info@gmail.com>
2023-12-16 15:27:22 +0800

    Change made by

--- src/App.tsx
+++ src/App.tsx
@@ -74,7 +74,7 @@ function App() {
         <Control onClick={increaseVolumes} icon={icon.volumeHigh} />
       </div>
       <div className="made-by">
-        <p>Crafted by <a href="mailto:alexandrochen.com@gmail.com">Alexandro Chen</a></p>
+        <p>Crafted by <a href="mailto:alexandrochen.com@gmail.com">Wei-Ying Chen</a></p>
       </div>
     </ThemeProvider>
   );
